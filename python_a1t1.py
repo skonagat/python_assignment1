91f45ff4f3d2cac6ccf951d494150e2f6e83bbda
skonagat <srinidhi.konagatta@gmail.com>
2025-07-10 19:19:14 +0530

    Add files via upload

---
+++ python_a1t1.py
@@ -0,0 +1,14 @@
+# Task 1 : Perform basic mathematical operations
+
+num1 = float(input ("Enter the first number : "))
+num2 = float(input ("Enter the second number : "))
+
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+
+print("Addition = ", add)
+print("Subtraction = ", sub)
+print("Multiplication = ", mul)
+print("Division = ", div)
---
+++ python_a1t2.py
@@ -0,0 +1,6 @@
+#Task 2 : Create a personalized greeting
+
+str1 = input("Enter your first name : ")
+str2 = input("Enter your second name : ")
+
+print ("Hello " + str1 + " " + str2 + ". Welcome to the Python program.")
